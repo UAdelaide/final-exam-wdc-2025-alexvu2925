944c79321bdfab9e9509ff6f226fd95cff97bc1e
alexvu2925 <alexwil2925@gmail.com>
2025-06-20 02:58:37 +0000

    [automatic commit] 2025-06-20 02:58:37 UTC 101.115.237.80

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,7 @@ router.post('/logout', (req, res) => {
   req.session.destroy(() => {
     res.clearCookie('connect.sid');
     res.json({ message: 'Logged out successfully' });
-    
+  });
 });
 
 module.exports = router;
